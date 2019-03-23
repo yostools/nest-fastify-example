5a0f7ee97049d2cd8371d3c27261a6c5ed650da4
Kai Haase <kai.haase@uni.tools>
2019-03-23 12:12:23 +0100

    Init via NestJS cli and added Fastify

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+// Bootstrap server
+async function bootstrap() {
+
+  // Init fasitfy
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule, new FastifyAdapter(),
+  );
+
+  // Enable CORS
+  app.enableCors();
+
+  // Start server
+  await app.listen(3000);
+}
+
+// Start server
+bootstrap();
